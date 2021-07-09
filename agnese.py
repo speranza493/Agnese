7217ac33250948c3e5d2e546ce7a6616001734ee
Marco Speranza <speranza.wix@gmail.com>
2021-07-09 09:03:26 +0200

    mase programma

--- agnese.py
+++ agnese.py
@@ -1,1 +1,18 @@
-fwdefce
+
+boolD = True
+def main(stringa):
+    pass
+
+
+
+
+if __name__ == "__main__":
+    if boolD:
+        print("START")
+    
+    string = "cococo"
+    main(string)
+    
+    
+    if boolD:
+        print("STOP")
