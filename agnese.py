edee2cd77b87db9129d84f1450e5d75dbd61443f
Marco Speranza <speranza.wix@gmail.com>
2021-07-09 17:17:52 +0200

    nuove modifiche per vedere le parole dietro

--- agnese.py
+++ agnese.py
@@ -8,8 +8,6 @@ def main(stringa):
     while (answer == False and x < length):
         letter = word[x]
         position = x
-        
-        
         for y in range(0,len(word)):
             check = controlIfTheStringExistAfterThat(letter,word,position)
             if (check == True):
@@ -26,30 +24,32 @@ def main(stringa):
     
 def controlIfTheStringExistAfterThat(letter,word,positions):
     lentgh = len(letter)
-    lentghSub1 = positions+lentgh-1
-    lentghSub2 = positions+lentgh+lentgh-1
+    lentghSub1 = positions+lentgh -1
+    lentghSub0 = ""
+    lentghSub2 = positions+lentgh+lentgh -1
+    existElement = checkIfTheElementExist(letter,word,positions)
+    if (existElement):
+        pass
+    if (lentgh == 1):
+        lentghSub1 = positions+lentgh 
+        lentghSub2 = positions+lentgh+lentgh  
     sub1 = word[lentghSub1:lentghSub2]
-    sub2 = word[lentghSub2:lentghSub2+lentgh-1]
+    sub2 = word[lentghSub2:lentghSub2+lentgh]
     answer = False
-    if (letter == sub1):
+    if (letter == sub1 and sub1 != sub2):
         answer = True
     if (letter == sub2 and letter == sub1 and sub1 == sub2):
         answer = False
     return answer
 
 
-def checkIfIAmAtTheStartOfTheString(position):
-    answer = False
-    if (position == 0):
-        answer = True
-    return answer
+def checkIfTheElementExist(letter,word,positions):
+    lentgh = len(letter)
+    risp = False
+    if (positions - lentgh >= 0):
+        risp = True
+    return risp
 
-
-def checkIfIAmAtTheEndOfTheString(position,word):
-    answer = False
-    if (position+1 == len(word)):
-        answer = True
-    return answer
 
 
 
@@ -57,7 +57,7 @@ if __name__ == "__main__":
     if boolD:
         print("START")
     
-    string = "tutucopa"
+    string = "cococo"
     main(string)
     
     
