6cdf11cae8efe49e49cda041d9faae4946ff9003
Marco Speranza <speranza493@gmail.com>
2021-07-15 00:11:33 +0200

    tentativo di creare una funzione corretta

--- agnese.cpp
+++ agnese.cpp
@@ -11,7 +11,7 @@ int main() {
     answer = controllingIfExistADouble(index,character);
     index++;
   };
-  
+
   return 0;
 }
 
@@ -20,6 +20,29 @@ bool controllingIfExistADouble(int index,string word){
   bool answer = false;
   int len = word.length();
   string substring = word.substr(index,word.length());
+  int indexWord = 0;
+  while (answer==false && indexWord < substring.length()){
+    int index_ = substring.find(char_);
+    bool answerForUndenstandHowManyElementAreInTheWord = controllingHowMuchTimeExistAString(substring,char_);
+    if (answerForUndenstandHowManyElementAreInTheWord==2){
+      answer = true;
+    }
+    char_ = char_+substring[indexWord];
+    indexWord++;
+  }
+  return answer;
+}
 
+bool controllingHowMuchTimeExistAString(string word,string char_){
+  int lenChar = char_.length();
+  int lenWord = word.length();
+  double lenOfTheWord = lenWord/lenChar;
+  bool answer = false;
+  int i = 0;
+  while (answer==false && i <= lenOfTheWord){
+
+    i++;
+  }
+  
   return answer;
 }
